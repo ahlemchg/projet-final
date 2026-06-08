dc0ded318e9cd9ad00896af1c49a7220eb5f2bb8
boutabacs <boutaba.cs@gmail.com>
2026-06-08 15:56:44 +0100

    version 3\4

--- Hubmarket/src/requestMethods.js
+++ Hubmarket/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api/";
 
 // Helper to get token safely from localStorage
 const getToken = () => {
